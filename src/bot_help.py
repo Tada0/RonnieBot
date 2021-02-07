570355c54ec26c8b24ccc43a0bfc7dadc51d01e1
Tada0 <tomekholda@gmail.com>
2021-02-07 22:30:04 +0100

    Update help

--- src/bot_help.py
+++ src/bot_help.py
@@ -8,7 +8,9 @@ Ronnie reacts to '!', '?' and '#' command prefixes, in order to be compatible wi
 ```- !ronnie_add - Adding ronnie to the voice channel
 - !ronnie_remove - Removing ronnie from the voice channel
 - !ronnie - Help
-- !ronnie_interval <Integer> - Setting interval between playing sounds (seconds)
+- !ronnie_interval <Integer> - Set interval between playing sounds (seconds)
+- !ronnie_quote_time <HH:MM> - Set time when bot will post inspirational quote
+- !ronnie_quote - Post inspirational quote
 - !ronnie_sound - Play random Ronnie sound
 - !ronnie_vibe_check - Vibe Check```
 **Additional Commands**
